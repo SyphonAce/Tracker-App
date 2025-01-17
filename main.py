c196b2b9542a874df6b82bde3afa1a251a6fe81f
Kayne <student.160774@worcesterschools.net>
2025-01-17 09:33:32 -0500

    Half Way Done

--- main.py
+++ main.py
@@ -8,10 +8,25 @@ class Agent:
         self.role = role
         self.abilities = abilities
         self.releaseDate = releaseDate
+
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Descirption: {self.description}")
+        print(f"Role: {self.role}")
+        print(f"Abilities: {self.abilities}")
+        print(f"Release Date: {self.releaseDate}")
         
 
+    def fetch_agent_data(agent_name):
+        url = f"https://valorant-api.com/v1/agents"
+        response = requests.get(url)
 
-url = f"https://valorant-api.com/v1/agents"
+        if response.status_code == 200:
+            agent_data = response.json()
+            return agent_data
+        else:
+            print(f"Error fetching data for {agent_name}.")
+            return None
 
 
-response = 
+
